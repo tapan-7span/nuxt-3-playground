6a937a756be7710980594b8282f058e86689ecd5
Tapan Padaliya <tapan@7span.com>
2023-10-19 13:44:19 +0530

    changes in template

--- app.vue
+++ app.vue
@@ -1,28 +1,9 @@
 <template>
-  <div class="text-red-400 text-2xl">Nuxt Testing App</div>
-  <div>
-    <button class="bg-gray-400 border-gray-700 rounded p-2" @click="fetchData">
-      Fetch Data
-    </button>
-  </div>
-  <div>
-    Response :
-    {{ responseData }}
+  <div class="text-red-400 flex items-center justify-center text-4xl">
+    Hello World
   </div>
 </template>
 <script>
-export default {
-  data() {
-    return { responseData: null };
-  },
-  created() {},
-  methods: {
-    async fetchData() {
-      const data = await this.$axios.$get("http://localhost:8008/items/blog");
-      
-      this.responseData = data?.data;
-    },
-  },
-};
+export default {};
 </script>
 <style lang=""></style>
