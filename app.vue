3c843c3511c4f4ee7d14e253e51cfb563f516fd0
Tapan Padaliya <tapan@7span.com>
2023-10-19 13:39:43 +0530

    init commit

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="text-red-400 text-2xl">Nuxt Testing App</div>
+  <div>
+    <button class="bg-gray-400 border-gray-700 rounded p-2" @click="fetchData">
+      Fetch Data
+    </button>
+  </div>
+  <div>
+    Response :
+    {{ responseData }}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return { responseData: null };
+  },
+  created() {},
+  methods: {
+    async fetchData() {
+      const data = await this.$axios.$get("http://localhost:8008/items/blog");
+      
+      this.responseData = data?.data;
+    },
+  },
+};
+</script>
+<style lang=""></style>
